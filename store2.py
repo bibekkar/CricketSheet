bee62a3c630c8d82547b19df982e718f2279fa24
Sumit Shyamsukha <sumitzster@gmail.com>
2015-03-27 21:41:00 +0530

    Third commit -- fixed store2

--- store2.py
+++ store2.py
@@ -1,3 +1,18 @@
+def store(deliveries, innings, db):
+	entries = []
+	for i in range(len(deliveries)):
+		keys = deliveries[i].keys()
+		for j in keys:
+			ball = deliveries[i][j]
+			bowler = ball['bowler']
+			batsman = ball['batsman']
+			runs = int(ball['runs']['batsman'])
+			extras = int(ball['runs']['extras'])
+			total = int(ball['runs']['total'])
+			ball=j
+			entries.append((innings,ball,bowler,batsman,extras,runs,total))
+	db.executemany("INSERT INTO balls VALUES(?,?,?,?,?,?,?)",entries)
+
 import yaml
 import sqlite3
 
@@ -7,13 +22,14 @@ file = open("335982.yaml", "r")
 data = yaml.load(file)
 
 # Creating db
-conn = sqlite3.connect('data.db')
+conn = sqlite3.connect('data2.db')
 db = conn.cursor()
 
 try:
 	# Creating table in db
 	db.execute('''CREATE TABLE balls (innings INT, ball TEXT, bowler TEXT, batsman TEXT, extras INT, runs INT, total INT)''')
-	
+
+
 	# Stores ball-by-ball data for both innings
 	innings = data['innings']
 	first_innings = innings[0]['1st innings']
@@ -28,35 +44,13 @@ try:
 
 	# Committing the changes made
 	conn.commit()
-except:
+except: 
 	print()
 
-# Number of dot balls bowled by KKR
-
-db.execute("SELECT * FROM innings WHERE team = 'Kolkata Knight Riders'")
-rows = db.fetchall()
-for r in rows:
-	print(r)
-
 # Querying the data
-db.execute('SELECT * FROM balls')
+db.execute('SELECT * FROM balls WHERE runs=6 AND innings=1')
 rows = db.fetchall()
 for r in rows:
 	print(r)
 
 conn.close()
-
-
-def store(deliveries, innings, db):
-	entries = []
-	for i in range(len(deliveries)):
-		keys = deliveries[i].keys()
-		for j in keys:
-			ball = deliveries[i][j]
-			bowler = ball['bowler']
-			batsman = ball['batsman']
-			runs = int(ball['runs']['batsman'])
-			extras = int(ball['runs']['extras'])
-			total = int(ball['runs']['total'])
-			entries.add((innings,j,bowler,batsman,extras,runs,total))
-	db.executeall("INSERT INTO balls VALUES (?,?,?,?,?,?,?)",entries)
